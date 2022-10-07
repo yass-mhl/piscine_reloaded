345b3f871841be8c39c8ab7895ccfde96edca4b4
Yassin Mehlil <yassin.mehlil@gmail.com>
2022-10-07 15:49:49 +0200

    ex12

---
+++ ex12/ft_iterative_factorial.c
@@ -0,0 +1,16 @@
+int	ft_iterative_factorial(int nb)
+{
+	int				i;
+	unsigned int	res;
+
+	if (nb < 0 || nb > 12)
+		return (0);
+	i = 2;
+	res = 1;
+	while (i <= nb)
+	{
+		res *= i;
+		++i;
+	}
+	return (res);
+}
